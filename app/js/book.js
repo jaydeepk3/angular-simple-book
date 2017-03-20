47b7f9ef4413bed46a5ce06091039c9ccb4f11d7
jd kataria <jd kataria>
2017-03-20 19:31:13 +0530

    book files uploaded.

---
+++ app/js/book.js
@@ -0,0 +1,39 @@
+(function() {
+	// body...
+	'use strict';
+	var app = angular.module('myApp',['ngRoute']).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+  $routeProvider.when('/details/:id',{
+  	templateUrl:'user.html',
+  	controller : 'BookController'
+  	
+  })
+  $routeProvider.when('/',{
+  	templateUrl:'books.html'
+  });
+  // $routeProvider.otherwise({redirectTo: 'index.html'});
+}]);
+	app.controller('BookController',['$http','$scope','$route','$routeParams',function($http,$scope,$route,$routeParams) {
+		var store = this;
+		store.product =[];
+		$http.get('http://localhost/book/book.json').success(function(data){
+         store.product = data;
+         console.log(data);
+
+         $scope.id = $routeParams.id;
+		console.log($scope.id);
+		 
+		});
+		}]);
+	app.controller('ScrollController', ['$scope', '$location', '$anchorScroll',function($scope, $location, $anchorScroll) {
+      // call $anchorScroll()
+      $anchorScroll();
+    }
+  ]);
+
+
+
+	
+	
+	})();
